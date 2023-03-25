ce271868b09fb179ea83de6ceb9029bc0f09c1be
Benoit <nau.benoit@gmail.com>
2023-03-25 15:19:58 -0400

    Singleton with ThreadSafe approach

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,25 @@
 ﻿namespace Singleton
 {
+	public class Logger
+	{
+		// Lazy => ThreadSafe approach
+		private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+		public static Logger Instance
+		{
+			get
+			{
+				return _lazyLogger.Value;
+			}
+		}
+
+		protected Logger() { }
+
+		public void Log(string message)
+		{
+			Console.WriteLine($"Message to log: {message}");
+		}
+	}
+
   
 }
