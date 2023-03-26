000f02ec3079302238433df718310339fd221806
Benoit <nau.benoit@gmail.com>
2023-03-26 15:28:29 -0400

    Abstract Factory

--- AbstractFactory/Implementation.cs
+++ AbstractFactory/Implementation.cs
@@ -1,4 +1,104 @@
 ﻿namespace AbstractFactory
 {
-   
+    public interface IShoppingCartPurchaseFactory
+    {
+        IDiscountService CreateDiscountService();
+
+        IShippingCostService CreateShippingCostService();
+    }
+
+    /// <summary>
+    /// Abstract product
+    /// </summary>
+    public interface IDiscountService
+    {
+        int DiscountPercentage { get; }
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class BelgiumDiscountService: IDiscountService
+    {
+        public int DiscountPercentage => 20;
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class FranceDiscountService: IDiscountService
+    {
+        public int DiscountPercentage => 10;
+    }
+
+    /// <summary>
+    /// Abstract product
+    /// </summary>
+    public interface IShippingCostService
+    {
+        decimal ShippingCosts { get; }
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class BelgiumShippingCostService: IShippingCostService
+    {
+        public decimal ShippingCosts => 20;
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class FranceShippingCostService: IShippingCostService
+    {
+        public decimal ShippingCosts => 25;
+    }
+
+    public class BelgiumShoppingCartPurchaseFactory: IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new BelgiumDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new BelgiumShippingCostService();
+        }
+    }
+
+    public class FranceShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new FranceDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new FranceShippingCostService();
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IShippingCostService _shippingCostService;
+        private int _orderCosts;
+
+        public ShoppingCart(IShoppingCartPurchaseFactory factory)
+        {
+            _discountService = factory.CreateDiscountService();
+            _shippingCostService= factory.CreateShippingCostService();
+            _orderCosts = 200;
+        }
+
+        public void CalculateCosts()
+        {
+            Console.WriteLine($"Total costs = " +
+                $"{_orderCosts - (_orderCosts / 100 * _discountService.DiscountPercentage) + _shippingCostService.ShippingCosts}");
+        }
+    }
+
 }
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.Title = "Abstract Factory";
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+var shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+shoppingCartForBelgium.CalculateCosts();
+
+var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+shoppingCartForFrance.CalculateCosts();
+
+Console.ReadKey();
