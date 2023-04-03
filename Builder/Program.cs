d1a60e899723ba7310e20f8f4537599500b27fb8
Benoit <nau.benoit@gmail.com>
2023-04-03 18:04:01 -0400

    Builder pattern

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.Title = "Builder";
+﻿
+using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+
+garage.Show();
+
+garage.Construct(bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+
+garage.Show();
+
+Console.ReadKey();
